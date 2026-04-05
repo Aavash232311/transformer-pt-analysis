eed6fd05c940aad295d8075836bf8b7a46adfbd2
avash <aavash2005@gmail.com>
2026-04-04 19:24:29 -0500

    Added: Discrete Fourier Transform and power spectrum

--- script/analysis/analysis_dft.py
+++ script/analysis/analysis_dft.py
@@ -30,5 +30,17 @@ class Analysis:
                     logits = self.model(input_ids)  # [1, seq_len, n]
                     self.logit_lattice[a, b] = logits[0, -1, :]
 
-        # ℓ̃(c|a,b) = ℓ(c|a,b) - (1/n) Σ ℓ(c'|a,b)
+        # ℓ̃ (c|a,b) = ℓ(c|a,b) - (1/n) Σ ℓ(c'|a,b) centered logits
         self.logit_lattice = self.logit_lattice - self.logit_lattice.mean(dim=-1, keepdim=True)
+
+        n = self.vocab_size
+
+        self.fourier_lattice = torch.zeros(n, n, n, dtype=torch.complex64)
+
+        for c in range(n):
+            phi = self.logit_lattice[:, :, c]          # shape [n, n] one class slice
+            self.fourier_lattice[:, :, c] = torch.fft.fft2(phi) / n   # eq. 17
+        
+        self.power_spectrum = self.fourier_lattice.abs() ** 2  # shape [n, n, n] eqn 16, basically squaring
+
+        return self.power_spectrum
