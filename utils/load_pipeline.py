69442ee9f19e586ace30c0aa5a2792ad601634a5
avash <aavash2005@gmail.com>
2026-04-14 15:37:54 -0500

    Fixed: Pair() function returning None

--- utils/load_pipeline.py
+++ utils/load_pipeline.py
@@ -3,10 +3,7 @@ from torch.utils.data import Dataset, DataLoader
 
 
 
-''' 
-    Use this if you have the pairs, to load it into a dataset
-
-'''
+''' Use this if you have the pairs, to load it into a dataset '''
 class AdhocTestDataset(Dataset):
     def __init__(self, sequences):
         self.samples = sequences  
@@ -17,10 +14,7 @@ class AdhocTestDataset(Dataset):
     def __getitem__(self, idx):
         return self.samples[idx]
 
-''' 
-    From x, y torch array for input output to data loader object.
-'''
-
+'''  From x, y torch array for input output to data loader object.'''
 def get_dataset(seq, batch_size, shuffle): # leave this fasle in eval.
     ds = AdhocTestDataset(seq)
     return DataLoader(ds, batch_size=batch_size, shuffle=shuffle)
@@ -40,14 +34,30 @@ def build_sequence(a, b, mod=10, seq_len=20):
     return x, y
 
 
-''' 
-    from (a, b) array pairs retruns x, and y
-'''
-
-
+''' from (a, b) array pairs retruns x, and y '''
 def pairs(arr, mod, seq_len):
     res = []
     for a, b in arr:
         x, y = build_sequence(a, b, mod=mod, seq_len=seq_len)
         res.append((x, y))
     return res
+
+
+''' This get's the missing pairs out of total possible combinations. '''
+def get_missing_pairs(mod=10, seq_length=1000):
+    seq = [1, 1]
+    while len(seq) < seq_length: # here just normal stuff, of generating fib
+        seq.append((seq[-1] + seq[-2]) % mod)
+    
+    found = set()
+    for i in range(len(seq) - 1): # here we do not one dublicates in set.
+        found.add((seq[i], seq[i+1]))
+    
+    all_pairs = {(a, b) for a in range(mod) for b in range(mod)} # here we have all the possible combinations that could be found
+    missing = all_pairs - found
+    
+    print(f"missing pairs {len(missing)} total")
+    print(f"non missing pairs: {len(found)}")
+    for p in sorted(missing):
+        print(f"  {p}")
+    return found, missing
